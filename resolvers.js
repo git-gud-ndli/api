31cb01cd9cf37bd14020a9859ebc35d4185ca5a4
Timothée Oliger <oliger.timothee@gmail.com>
2018-12-07 00:00:50 +0100

    merge

--- resolvers.js
+++ resolvers.js
@@ -4,7 +4,6 @@ const bcrypt = require('bcryptjs');
 const uuid = require('uuid/v1');
 
 const pg = require('knex')(require('./knexfile').development);
-const uuid = require("uuid/v1");
 const models = require('./models');
 
 const data = {
@@ -59,40 +58,28 @@ for (const list of lists) {
 
 module.exports = {
   Query: {
-    todo: async (_, { id }, { dataSources }) => {
+    todo: async (_, {id}, {dataSources}) => {
       return todos.find(t => t.id === id);
     },
-    todoList: async (_, { id }, { dataSources }) => {
+    todoList: async (_, {id}, {dataSources}) => {
       return lists.find(l => l.id === id);
     },
-    user: async (_, { id }, { dataSources }) => {
+    user: async (_, {id}, {dataSources}) => {
       if (id === data.me.id) return data.me;
       return null;
     },
-    me: async (_, { }, { dataSources }) => {
+    me: async (_, {}, {dataSources}) => {
       return data.me;
     },
   },
   Mutation: {
-<<<<<<< HEAD
     login: async (_, {email, password}, {dataSources}) => {
-      const hash = bcrypt.hashSync(password, 8);
-      const users = await pg('users').where({
-        email,
-      });
-      if (users.length < 1) throw new Error('user not found');
-      const user = users[0];
-      if (!(await bcrypt.compare(password, user.password))) {
-        throw new Error('bad credentials');
-=======
-    login: async (_, { email, password }, { dataSources }) => {
-
       try {
         const user = await models.User.where({
           email,
         }).fetch();
 
-        if (!await bcrypt.compare(password, user.get('password'))) {
+        if (!(await bcrypt.compare(password, user.get('password')))) {
           throw new Error('bad credentials');
         }
 
@@ -106,9 +93,7 @@ module.exports = {
         );
       } catch (e) {
         throw new Error('login failed');
->>>>>>> b58e3adce5b883b619b942f94abbd70948f06d29
       }
-
     },
     register: async (_, {email, password}, {dataSources}) => {
       const hash = bcrypt.hashSync(password, 8);
@@ -132,23 +117,13 @@ module.exports = {
         throw new Error('could not create user: it may already exists');
       }
     },
-<<<<<<< HEAD
     todoCheck: async (_, {uuid, value}, {dataSources}) => {
-      console.log(uuid, value);
-      await pg('todos')
-        .where('id', uuid)
-        .update({
-          checked: value,
-        });
-=======
-    todoCheck: async (_, { uuid, value }, { dataSources }) => {
-      const todo = await models.Todo.where({ id: uuid }).fetch();
+      const todo = await models.Todo.where({id: uuid}).fetch();
       todo.checked = value;
       todo.save();
->>>>>>> b58e3adce5b883b619b942f94abbd70948f06d29
       return true;
     },
-    updateCoords: async (_, { lat, long }, { dataSources }) => {
+    updateCoords: async (_, {lat, long}, {dataSources}) => {
       return true;
     },
   },
