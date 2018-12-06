790f6c20ade0d4beecaccd5dab0b4a4d5478117e
ludovicm67 <ludovicmuller1@gmail.com>
2018-12-06 21:07:24 +0100

    created users migrations

--- migrations/20181206204054_create_users_table.js
+++ migrations/20181206204054_create_users_table.js
@@ -1,6 +1,6 @@
 
 exports.up = function(knex, Promise) {
-  knex.schema.createTable('users', function (table) {
+  return knex.schema.createTable('users', function (table) {
     table.increments();
     table.string('username');
     table.string('name');
@@ -11,5 +11,5 @@ exports.up = function(knex, Promise) {
 };
 
 exports.down = function(knex, Promise) {
-  knex.schema.dropTable('users');
+  return knex.schema.dropTable('users');
 };
